db4042cbde1406cf6d709255b9ad63c1ca385df5
jiyeon <jiyeon08.you@samsung.com>
2024-07-05 10:03:46 +0900

    [feature 1] ReadFive

--- DeviceDriveKATA/DeviceDriver.cpp
+++ DeviceDriveKATA/DeviceDriver.cpp
@@ -6,8 +6,11 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 //app이 read할 예정. app 클래스 만들어야함.
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int val;
+    for (int i = 0; i < 5; i++) {
+        val = m_hardware->read(address);
+    }
+    return val;
 }
 
 void DeviceDriver::write(long address, int data)
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,21 @@
-#include "pch.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../DeviceDriveKATA/FlashMemoryDevice.h"
+#include "../DeviceDriveKATA/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class FlashMemoryDeviceMock : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+TEST(TestCaseName, ReadFive) {
+	FlashMemoryDeviceMock mk;
+	DeviceDriver driver(&mk);
+
+	long address = 0x1;
+
+	EXPECT_CALL(mk, read)
+		.Times(5);
+	driver.read(address);
 }
